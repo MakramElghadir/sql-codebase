716cfe92bf298451f6164f9a19557d8c81489475
Makram <Mkelghadir@gmail.com>
2024-04-09 14:44:54 +0200

    AÑADIDO SQLITE

--- pelicula.sql
+++ pelicula.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS pelicula;
 CREATE TABLE 'personas' (
     'id' INTEGER NOT NULL UNIQUE,
     'nombre' TEXT NOT NULL UNIQUE,
-    'peliculaFavorita' TEXT NOT NULL
-    PRIMARY KEY ('id' AUTOINCREMENT)
+    'peliculaFavorita' TEXT NOT NULL,    PRIMARY KEY ('id' AUTOINCREMENT),
+    FOREIGN KEY (peliculaFavorita) REFERENCES pelicula(id)
 )STRICT;
 
 
@@ -14,9 +14,21 @@ CREATE TABLE 'pelicula' (
     'nombrePelucla' TEXT NOT NULL,
     'genero' TEXT NOT NULL,
     'ano' INTEGER,
-    'duracionEnMinutos' INTEGER
+    'duracionEnMinutos' INTEGER,
+    PRIMARY KEY ('id' AUTOINCREMENT)
+)STRICT;
+
+CREATE TABLE 'listaDePelicula' (
+    'id' INTEGER NOT NULL UNIQUE,
+    'listaDePelucla' 
 )STRICT;
 
 INSERT INTO personas (id, nombre, peliculaFavorita) VALUES(1, 'Rico', 'Iluminados por el fuego');
-INSERT INTO pelicula (id, nombrePelucla, genero, ano, duracionEnMinutos) VALUES(1, 'Iluminados por el fuego', 'drama/guerra', 2005, 105);
+
+INSERT INTO pelicula (id, nombrePelucla, genero, ano, duracionEnMinutos) VALUES(1, 'Iluminados por el fuego', 'historico/drama/guerra', 2005, 105);
+INSERT INTO pelicula (id, nombrePelucla, genero, ano, duracionEnMinutos) VALUES(2, 'Neuilly sa mere', 'Comedia', 2009, 90);
+INSERT INTO pelicula (id, nombrePelucla, genero, ano, duracionEnMinutos) VALUES(3, 'starship trooper', 'cienca ficcion militar', 1997, 129);
+INSERT INTO pelicula (id, nombrePelucla, genero, ano, duracionEnMinutos) VALUES(4, 'Five Nights at Freddys', 'horror', 2023, 109);
+INSERT INTO pelicula (id, nombrePelucla, genero, ano, duracionEnMinutos) VALUES(5, 'Company of Heroes', 'historico/guerra', 2013, 100);
+INSERT INTO pelicula (id, nombrePelucla, genero, ano, duracionEnMinutos) VALUES(6, 'Pieles', 'comedia/drama', 2017, 77);
 
